31432d34fd1613ff6d7a09bad89163172e03127a
Sam <saml@rjwilber.co.uk>
2025-10-23 16:22:42 +0100

    Adding range to the Spear Orc

--- statsMap.py
+++ statsMap.py
@@ -9,7 +9,7 @@ statsMap = {
     "Wall":        {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"resistance":8},
     "Ditch":       {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":0,"atkspeed":6,"range":50,"resistance":8},
     "Goblin":      {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":5,"range":50,"visibleRange":250},
-    "Spear Orc":   {"health":10,"damage":3,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":0.5,"range":50,"visibleRange":250},
+    "Spear Orc":   {"health":10,"damage":3,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":0.5,"range":100,"visibleRange":250},
     "Dark Knight": {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":6,"range":50,"visibleRange":250},
     "Giant Ogre":  {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":6,"range":50,"visibleRange":250},
     "Necromancer": {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":5,"atkspeed":6,"range":50,"spawn number":8,"visibleRange":250},
