12fc506fd636599528f1d3df3bd81218941d0084
Sam <saml@rjwilber.co.uk>
2025-11-02 17:57:07 +0000

    Slight rebalancing of units

--- statsMap.py
+++ statsMap.py
@@ -1,18 +1,25 @@
 statsMap = {
-    "Swordsman":   {"health":10,"damage":2,"cost":5,"type":"unit" ,"speed":0.2,"atkspeed":0.5,"range":50,"visibleRange":200},
-    "Archer":      {"health":10,"damage":2,"cost":1,"type":"unit","speed":0.1,"atkspeed":0.6,"range":100,"visibleRange":250},
-    "Cavalry":     {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
-    "Assassin":    {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
-    "ArcherTower": {"health":10,"damage":1,"cost":1,"type":"unit" ,"speed":5,"atkspeed":1.5,"range":150,"visibleRange":250},
-    "Catapult":    {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
-    "Fire Tower":  {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"AOE range":8,"visibleRange":250},
-    "Wall":        {"health":50,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"resistance":0.5},
-    "Ditch":       {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":0,"atkspeed":6,"range":50,"resistance":0},
-    "Goblin":      {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.2,"atkspeed":5,"range":50,"visibleRange":250},
-    "Spear Orc":   {"health":10,"damage":3,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":0.5,"range":100,"visibleRange":250},
-    "Dark Knight": {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":6,"range":50,"visibleRange":250},
+    "Swordsman":   {"health":5,"damage":2,"cost":5,"type":"unit" ,"speed":0.2,"atkspeed":0.5,"range":50,"visibleRange":200},
+    "Archer":      {"health":3,"damage":2,"cost":6,"type":"unit","speed":0.1,"atkspeed":0.6,"range":100,"visibleRange":250},
+    "ArcherTower": {"health":10,"damage":1,"cost":10,"type":"unit" ,"speed":5,"atkspeed":1.5,"range":150,"visibleRange":250},
+    "Wall":        {"health":50,"damage":2,"cost":10,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"resistance":0},#resistance must be decimal between 0 and 1
+    
+    "Goblin":      {"health":3,"damage":2,"cost":1,"type":"enemy" ,"speed":0.2,"atkspeed":0.5,"range":50,"visibleRange":250},
+    "Spear Orc":   {"health":3,"damage":3,"cost":1,"type":"enemy" ,"speed":0.2,"atkspeed":0.75,"range":100,"visibleRange":250},
+    "Dark Knight": {"health":10,"damage":4,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":6,"range":50,"visibleRange":250},
     "Giant Ogre":  {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":0.1,"atkspeed":6,"range":50,"visibleRange":250},
-    "Necromancer": {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":5,"atkspeed":6,"range":50,"spawn number":8,"visibleRange":250},
-    "Skeleton":    {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
+
     "Castle":      {"health":30,"damage":2,"cost":0,"type":"unit","speed":0,"atkspeed":0,"range":0,"resistance":0}
 }
+
+#"Cavalry":     {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
+#"Assassin":    {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
+# "Catapult":    {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
+# "Fire Tower":  {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":5,"atkspeed":6,"range":50,"AOE range":8,"visibleRange":250},
+# "Ditch":       {"health":10,"damage":2,"cost":1,"type":"unit" ,"speed":0,"atkspeed":6,"range":50,"resistance":0},
+# 
+# "Necromancer": {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":5,"atkspeed":6,"range":50,"spawn number":8,"visibleRange":250},
+# "Skeleton":    {"health":10,"damage":2,"cost":1,"type":"enemy" ,"speed":5,"atkspeed":6,"range":50,"visibleRange":250},
+# 
+
+
